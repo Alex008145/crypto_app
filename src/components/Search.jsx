fb9e6af008ebb339027fba569b24119b860c679f
Alexey <alex008145@gmail.com>
2024-07-17 19:42:19 +0300

    little changes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,71 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { CryptoContext } from "../context/CryptoContext";
+import debounce from "lodash.debounce";
+
+const SearchInput = ({ handleSearch }) => {
+  const [searchText, setSearchText] = useState("");
+  let { searchData } = useContext(CryptoContext);
+
+  let inputHandler = (e) => {
+    e.preventDefault();
+    let query = e.target.value;
+
+    setSearchText(query);
+    handleSearch(query);
+  };
+
+  return (
+    <>
+      <form className="relative flex items-center ml-7 w-96 font-nunito ">
+        <input
+          type="text"
+          name="search"
+          onChange={inputHandler}
+          value={searchText}
+          className="w-full pl-2 bg-gray-200 border border-transparent rounded-lg placeholder:text-gray-100 required outline-0 focus:border-cyan"
+          placeholder="search for coins..."
+        />
+        <button type="submit" className="absolute cursor-pointer right-1">
+          <FaSearch className="w-full h-auto" />
+        </button>
+      </form>
+      ;
+      {searchText.length > 0 ? (
+        <ul className="absolute right-0 w-full py-2 overflow-x-hidden bg-gray-200 rounded-lg top-11 h-96 bg-opacity-60 backdrop-blur-md">
+          {searchData ? (
+            searchData.map((coin) => {
+              return (
+                <li className="flex items-center my-2 ml-4 cursor-pointer">
+                  <img
+                    className="w-[2rem] h-[2rem] mx-1.5"
+                    src={coin.thumb}
+                    alt={coin.name}
+                  />
+                  <span>{coin.name}</span>
+                </li>
+              );
+            })
+          ) : (
+            <h2>Please wait...</h2>
+          )}
+        </ul>
+      ) : null}
+    </>
+  );
+};
 
 const Search = () => {
+  let { getSearchResult } = useContext(CryptoContext);
+
+  const debounceFunc = debounce(function (value) {
+    getSearchResult(value);
+  }, 2000);
+
   return (
-    <form className="relative flex items-center ml-7 w-96 font-nunito ">
-      <input
-        type="text"
-        name="search"
-        className="w-full pl-2 bg-gray-200 border border-transparent rounded-lg placeholder:text-gray-100 required outline-0 focus:border-cyan"
-        placeholder="search for coins..."
-      />
-      <button type="submit">
-        <FaSearch />
-      </button>
-    </form>
+    <>
+      <SearchInput handleSearch={debounceFunc} />
+    </>
   );
 };
 
--- src/context/CryptoContext.jsx
+++ src/context/CryptoContext.jsx
@@ -5,7 +5,8 @@ export const CryptoContext = createContext({});
 
 //create the provider component
 export const CryptoProvider = ({ children }) => {
-  const [cryptoData, setCryptoData] = useState({});
+  const [cryptoData, setCryptoData] = useState();
+  const [searchData, setSearchData] = useState();
 
   const getCryptoData = async () => {
     try {
@@ -22,12 +23,27 @@ export const CryptoProvider = ({ children }) => {
     }
   };
 
+  const getSearchResult = async (query) => {
+    try {
+      const data = await fetch(
+        `https://api.coingecko.com/api/v3/search?query=${query}`
+      )
+        .then((res) => res.json())
+        .then((json) => json);
+
+      console.log(data);
+      setSearchData(data.coins);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useLayoutEffect(() => {
     getCryptoData();
   }, []);
 
   return (
-    <CryptoContext.Provider value={{ cryptoData }}>
+    <CryptoContext.Provider value={{ cryptoData, searchData, getSearchResult }}>
       {children}
     </CryptoContext.Provider>
   );
